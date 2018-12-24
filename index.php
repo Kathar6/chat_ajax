1f9b12a86be27b8a7964c29e143054153ecd01b2
mate458 <matepabon@gmail.com>
2018-12-24 14:26:09 -0500

    estilos actualizados

--- index.php
+++ index.php
@@ -15,16 +15,17 @@
     <section>
         <div id="contenedor">
             <div id="cajachat">
-                <div id="chat">
-                    <span>User 1: </span>
+                <div id="datos-chat">
+                    <span style="color:#9e9e9e; margin-right:10px;">User 1: </span>
                     <span>Sample text</span>
-                    <span>10:04 am</span>
+                    <span style="float:right;">10:04 am</span>
                 </div>
             </div>
             <form action="index.php" method="post">
                 <input type="text" name="nombre" id="" placeholder="Ingresa tu nombre"/>
+                <br>
                 <textarea name="mensaje" id="" placeholder="Ingresa tu mensaje"></textarea>
-                <input type="submit" value="enviar" name="btnEnviar"/>
+                <input type="submit" value="Enviar" name="btnEnviar"/>
 
             </form>
         </div>
